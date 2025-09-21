6398f14b47ca3ca0341a6f2f544c1720cb96ba53
Olaleye Fuad <heniolarr@gmail.com>
2025-09-21 13:50:58 +0100

    First commit

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='p-4'>
+        <div className='border-[rgb(177,167,166)] border-b pb-4 pl-2 flex flex-row items-center justify-between relative'>
+            <div className='flex flex-row items-center space-x-2'>
+                <p className='font-bold font-inter text-lg md:text-2xl text-[#a4161a]'>myrequest.com</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,41 @@
+"use client"
 import Image from "next/image";
+import image from "@/app/assets/soundless.jpeg"
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [popup, showPopup] = useState(false)
+  const showPopUp = () => showPopup(!popup)
+  const router = useRouter()
+
+  return (
+    <div className="font-inter p-4">
+      Select a request room to make your request
+      <div className='w-full h-fit relative'>
+        <Image src={image} height={200} width={200} alt="Soundless" className="rounded-md mt-4"  />
+        <button 
+          onClick={showPopUp}
+          className="absolute bottom-2 left-10 mx-auto shadow-md bg-[#a4161a] text-white font-bold text-md rounded-4xl h-12 px-4"
+        >
+          Enter room
+        </button>
+      </div>
+      <div className="fixed top-0 right-0 h-screen w-screen  bg-black/50 flex items-center justify-center">
+        <div className="rounded-md bg-white shadow-md h-fit w-4/5 z-100 p-5">
+          <label>Enter room pin for Soundless:</label>
+          <input type="number" className="border-1 border-[#d3d3d3] mt-2 rounded-md w-full pl-2 py-1" />
+          <button 
+            onClick={() => router.push("/request")}
+            className="mt-3 bg-[#a4161a] text-white font-bold text-md rounded-md h-8 px-4"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Enter
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
+ 
